53e9da8a967f03f20ee5db60d53e4edf93ff9079
Ilya Krylov <ilya.krylov@intel.com>
2020-04-22 16:11:44 +0300

    fast normalized fusion

--- mmdet/models/necks/bifpn.py
+++ mmdet/models/necks/bifpn.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import xavier_init
@@ -19,18 +20,34 @@ class DepthwiseSeparableConv(nn.Module):
         out = self.pointwise(out)
         return out
 
+
 class BiFPNBlock(nn.Module):
 
     def __init__(self, num_outs, channels, conv_cfg, norm_cfg, activation):
         super().__init__()
+        self.epsilon = 0.0001
+
         self.channels = channels
         self.num_outs = num_outs
         self.activation = activation
 
         assert self.num_outs >= 3
 
-        self.lateral_convs_from_input = nn.ModuleList()
-        self.lateral_convs_from_top_down = nn.ModuleList()
+        self.lc_from_input = nn.ModuleList()
+        self.lc_from_top_down = nn.ModuleList()
+        self.fc_top_down = nn.ModuleList()
+        self.fc_bottom_up = nn.ModuleList()
+
+        self.lc_from_input_w = nn.ParameterList()
+        self.lc_from_top_down_w = nn.ParameterList()
+        self.top_down_w = nn.ParameterList()
+        self.bottom_up_w = nn.ParameterList()
+        self.shortcuts_w = nn.ParameterList()
+        for i in range(self.num_outs - 1):
+            self.top_down_w.append(nn.Parameter(torch.ones(1)))
+            self.bottom_up_w.append(nn.Parameter(torch.ones(1)))
+            if i > 0:
+                self.shortcuts_w.append(nn.Parameter(torch.ones(1)))
 
         for _ in range(self.num_outs - 1):
             conv = ConvModule(
@@ -41,7 +58,8 @@ class BiFPNBlock(nn.Module):
                 norm_cfg=norm_cfg,
                 activation=self.activation,
                 inplace=False)
-            self.lateral_convs_from_input.append(conv)
+            self.lc_from_input.append(conv)
+            self.lc_from_input_w.append(nn.Parameter(torch.ones(1)))
 
         for _ in range(self.num_outs - 1):
             conv = ConvModule(
@@ -52,29 +70,64 @@ class BiFPNBlock(nn.Module):
                 norm_cfg=norm_cfg,
                 activation=self.activation,
                 inplace=False)
-            self.lateral_convs_from_top_down.append(conv)
+            self.lc_from_top_down.append(conv)
+            self.lc_from_top_down_w.append(nn.Parameter(torch.ones(1)))
+
+        for _ in range(self.num_outs - 1):
+            self.fc_top_down.append(DepthwiseSeparableConv(channels, channels))
+            self.fc_bottom_up.append(DepthwiseSeparableConv(channels, channels))
 
     def forward(self, inputs):
-        outputs = [
-            self.lateral_convs_from_input[i](input) if i < self.num_outs - 1 else input
-            for i, input in enumerate(inputs)
-        ]
 
-        for i in range(len(outputs) - 1, 0, -1):
-            outputs[i - 1] += F.interpolate(outputs[i], scale_factor=2, mode='nearest')
+        top_down = []
+        top_down_weights_sum = []
 
-        outputs = [
-            self.lateral_convs_from_top_down[i - 1](output) if i > 0 else output
-            for i, output in enumerate(outputs)
-        ]
+        # Compute lateral convolutions on input
+        for i in range(self.num_outs - 1):
+            w = F.relu(self.lc_from_input_w[i])
+            top_down.append(w * self.lc_from_input[i](inputs[i]))
+            top_down_weights_sum.append(w)
+        top_down.append(inputs[-1])
 
-        for i in range(1, len(outputs)):
-            outputs[i] += F.interpolate(outputs[i - 1], scale_factor=0.5, mode='nearest')
+        # Compute top-down path
+        for i in range(len(top_down) - 1, 0, -1):
+            w = F.relu(self.top_down_w[i - 1])
+            top_down[i - 1] = top_down[i - 1] + w * F.interpolate(top_down[i], scale_factor=2,
+                                                                  mode='nearest')
+            top_down_weights_sum[i - 1] = top_down_weights_sum[i - 1] + w
 
-        for i in range(1, len(outputs) - 1):
-            outputs[i] += inputs[i]
+        # Fuse lateral and top-down
+        for i in range(self.num_outs - 1):
+            top_down[i] = self.fc_top_down[i](
+                top_down[i] / (top_down_weights_sum[i] + self.epsilon))
 
-        return tuple(outputs)
+        bottom_up = [top_down[0], ]
+        bottom_up_weights_sum = [None, ]
+        # Compute lateral convolutions on top-down
+        for i in range(1, self.num_outs):
+            w = F.relu(self.lc_from_top_down_w[i - 1])
+            bottom_up.append(w * self.lc_from_top_down[i - 1](top_down[i]))
+            bottom_up_weights_sum.append(w)
+
+        # Compute bottom-up path
+        for i in range(1, len(bottom_up)):
+            w = F.relu(self.bottom_up_w[i - 1])
+            bottom_up[i] = bottom_up[i] + w * F.interpolate(bottom_up[i - 1], scale_factor=0.5,
+                                                            mode='nearest')
+            bottom_up_weights_sum[i] = bottom_up_weights_sum[i] + w
+
+        # Add shortcuts
+        for i in range(1, len(bottom_up) - 1):
+            w = F.relu(self.shortcuts_w[i - 1])
+            bottom_up[i] = bottom_up[i] + w * inputs[i]
+            bottom_up_weights_sum[i] = bottom_up_weights_sum[i] + w
+
+        # Fuse
+        for i in range(1, len(bottom_up)):
+            bottom_up[i] = self.fc_bottom_up[i - 1](
+                bottom_up[i] / (bottom_up_weights_sum[i] + self.epsilon))
+
+        return tuple(bottom_up)
 
 
 @NECKS.register_module
@@ -137,7 +190,7 @@ class BiFPN(nn.Module):
     @auto_fp16()
     def forward(self, inputs):
         assert len(inputs) == len(self.in_channels)
-        
+
         extra_inputs = []
         for i in range(self.num_outs - len(self.in_channels)):
             if i == 0:
