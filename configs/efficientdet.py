6c9e5ca6221b062a2db53656469e0da31e9abcc8
Ilya Krylov <ilya.krylov@intel.com>
2020-04-27 15:15:22 +0300

    atss

--- configs/efficientdet.py
+++ configs/efficientdet.py
@@ -1,7 +1,7 @@
 # model settings
 input_size = 512
 model = dict(
-    type='FCOS',
+    type='ATSS',
     backbone=dict(
         type='efficientnet_b0b',
         out_indices=(3, 4, 5),
@@ -13,7 +13,7 @@ model = dict(
     neck=dict(
         type='BiFPN',
         in_channels=[40, 112, 320],
-        out_channels=64,
+        out_channels=128,
         num_outs=5,
         num_layers=3,
         # start_level=0,
@@ -23,61 +23,56 @@ model = dict(
         # relu_before_extra_convs=True
     ),
     bbox_head=dict(
-        type='FCOSHead',
+        type='ATSSHead',
         num_classes=81,
-        in_channels=64,
+        in_channels=128,
         stacked_convs=3,
-        feat_channels=64,
-        strides=(8, 16, 32, 64, 128),
+        feat_channels=128,
+        octave_base_scale=8,
+        scales_per_octave=1,
+        anchor_ratios=[1.0],
+        anchor_strides=[8, 16, 32, 64, 128],
+        target_means=[.0, .0, .0, .0],
+        target_stds=[0.1, 0.1, 0.2, 0.2],
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
             loss_weight=1.0),
-        loss_bbox=dict(type='IoULoss', loss_weight=1.0),
+        loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
         loss_centerness=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)))
 # training and testing settings
 cudnn_benchmark = True
 train_cfg = dict(
-    assigner=dict(
-        type='MaxIoUAssigner',
-        pos_iou_thr=0.4,
-        neg_iou_thr=0.4,
-        min_pos_iou=0.,
-        ignore_iof_thr=-1,
-        gt_max_assign_all=False),
-    smoothl1_beta=1.,
-    use_giou=False,
-    use_focal=False,
+    assigner=dict(type='ATSSAssigner', topk=9),
     allowed_border=-1,
     pos_weight=-1,
-    neg_pos_ratio=3,
     debug=False)
 test_cfg = dict(
-    nms=dict(type='nms', iou_thr=0.45),
+    nms=dict(type='nms', iou_thr=0.5),
     min_bbox_size=0,
     score_thr=0.02,
     max_per_img=200)
 # model training and testing settings
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/home/ikrylov/data/object_detection/coco2017/'
+data_root = '/home/ikrylov/datasets/coco/'
 img_norm_cfg = dict(mean=[0, 0, 0], std=[255, 255, 255], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile', to_float32=True),
     dict(type='LoadAnnotations', with_bbox=True),
-    # dict(
-    #     type='PhotoMetricDistortion',
-    #     brightness_delta=32,
-    #     contrast_range=(0.5, 1.5),
-    #     saturation_range=(0.5, 1.5),
-    #     hue_delta=18),
-    # dict(
-    #     type='MinIoURandomCrop',
-    #     min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
-    #     min_crop_size=0.1),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.1),
     dict(type='Resize', img_scale=(input_size, input_size), keep_ratio=False),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='RandomFlip', flip_ratio=0.0),
@@ -98,15 +93,15 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=5,
-    workers_per_gpu=4,
+    imgs_per_gpu=12,
+    workers_per_gpu=0,
     train=dict(
         type='RepeatDataset',
-        times=1,
+        times=10,
         dataset=dict(
             type=dataset_type,
-            ann_file=data_root + 'annotations/instances_val2017.json',
-            img_prefix=data_root + 'val2017',
+            ann_file=data_root + 'annotations/instances_train2017.json',
+            img_prefix=data_root + 'train2017',
             pipeline=train_pipeline
         )
     ),
@@ -130,8 +125,8 @@ lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=1200,
-    warmup_ratio=1.0 / 3,
-    step=[2, 4, 6])
+    warmup_ratio=1.0 / 300,
+    step=[4, 5, 6, 7])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
@@ -142,7 +137,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 10
+total_epochs = 8
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = 'outputs'
