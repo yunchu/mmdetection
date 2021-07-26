9537c3fece60543b91b73c51b894d47bd96e7198
Dmitry Sidnev <dmitry.sidnev@intel.com>
2021-07-26 12:32:26 +0300

    Fix mmcv min version

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -16,7 +16,7 @@ def digit_version(version_str):
 
 
 mmcv_minimum_version = '1.2.4'
-mmcv_maximum_version = '1.3.1'
+mmcv_maximum_version = '1.3.9'
 mmcv_version = digit_version(mmcv.__version__)
 
 
