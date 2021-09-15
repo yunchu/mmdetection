54a9e4c4582957cb5ea72a30b5c6e75b45823332
Ilya Krylov <ilya.krylov@intel.com>
2021-09-15 14:50:10 +0300

    t11

--- tests/test_ote_api2.py
+++ tests/test_ote_api2.py
@@ -4,49 +4,28 @@ import unittest
 from copy import deepcopy
 
 import numpy as np
-
-from ote_sdk.entities.annotation import AnnotationSceneKind
-from ote_sdk.entities.subset import Subset
-from ote_sdk.entities.model_template import parse_model_template
-from ote_sdk.tests.test_helpers import generate_random_annotated_image
-
-from sc_sdk.entities.annotation import AnnotationScene, NullAnnotationScene
-from sc_sdk.entities.dataset_item import DatasetItem
-from sc_sdk.entities.datasets import Dataset, NullDatasetStorage
-from sc_sdk.entities.image import Image
-from sc_sdk.entities.media_identifier import ImageIdentifier
-
 from e2e_test_system import e2e_pytest_api
-
-
+from ote_sdk.entities.model_template import parse_model_template
+from ote_sdk.entities.subset import Subset
+from sc_sdk.entities.annotation import NullAnnotationScene
+from sc_sdk.entities.dataset_item import DatasetItem
+from sc_sdk.entities.dataset_storage import NullDatasetStorage
+from sc_sdk.entities.datasets import Dataset, DatasetItem, NullDatasetStorage
+from sc_sdk.entities.image import Image
 from sc_sdk.usecases.repos import ImageRepo
-
-from sc_sdk.tests.test_helpers import generate_random_annotated_image
-
 from sc_sdk.usecases.repos.workspace_repo import WorkspaceRepo
 from sc_sdk.utils.project_factory import ProjectFactory
 
-from ote_sdk.entities.annotation import AnnotationSceneKind
-from ote_sdk.entities.subset import Subset
 
-from sc_sdk.entities.annotation import AnnotationScene
-from sc_sdk.entities.datasets import Dataset, DatasetItem
-from sc_sdk.entities.dataset_storage import NullDatasetStorage
-from sc_sdk.entities.image import Image
-
-
-class OTEDataset2:
-
-    def __init__(self, ote_dataset: Dataset, classes=None):
-        self.ote_dataset = ote_dataset
-        self.CLASSES = classes
-        self.keys = ['img']
+class ObjectDetectionDataset:
+    def __init__(self, sc_dataset: Dataset):
+        self.sc_dataset = sc_dataset
 
     def __len__(self):
-            return len(self.ote_dataset)
+            return len(self.sc_dataset)
 
     def __getitem__(self, idx):
-        item = self.ote_dataset[idx]
+        item = self.sc_dataset[idx]
         data_info = dict(dataset_item=item, width=item.width, height=item.height)
 
         data = deepcopy(data_info)
@@ -54,8 +33,7 @@ class OTEDataset2:
         dataset_item = data['dataset_item']
         img = dataset_item.numpy
 
-        assert img.shape[0] == data['height']
-        assert img.shape[1] == data['width']
+        assert img.shape[0:2] == (data['height'], data['width'])
 
         data['img'] = img
 
@@ -63,6 +41,7 @@ class OTEDataset2:
         new_data['img'] = data['img']
 
         return new_data
+
 
 class API(unittest.TestCase):
 
@@ -85,28 +64,26 @@ class API(unittest.TestCase):
         items = []
 
         for i in range(0, number_of_images):
-            image_numpy = (np.random.rand(random.randrange(200, 500), random.randrange(200, 500), 3) * 225).astype(np.uint8)
+            image_numpy = (np.random.rand(random.randrange(200, 500), random.randrange(200, 500), 3) * 255).astype(np.uint8)
             image = Image(
-                name=f"image {i}", dataset_storage=project.dataset_storage, numpy=image_numpy
+                name=f"image{i}", dataset_storage=project.dataset_storage, numpy=image_numpy,
+                #id=f"image{i}"
             )
-            items.append(DatasetItem(media=image, annotation_scene=NullAnnotationScene()))
+            items.append(DatasetItem(media=image, annotation_scene=NullAnnotationScene(), subset=Subset.TRAINING))
 
         if save_to_repos:
             for i, item in enumerate(items):
                 ImageRepo(project.dataset_storage).save(item.media)
 
-        for i, _ in enumerate(items):
-            items[i].subset = Subset.TRAINING
-
-        dataset = Dataset(NullDatasetStorage(), items)
+        dataset = Dataset(project.dataset_storage, items)
         train_dataset = dataset.get_subset(Subset.TRAINING)
 
-        mm_train_dataset = OTEDataset2(train_dataset, classes)
+        object_detection_dataset = ObjectDetectionDataset(train_dataset)
 
         items = []
         i = 0
-        for a in range(100):
-            for item in mm_train_dataset:
+        for _ in range(100):
+            for item in object_detection_dataset:
                 items.append(item)
                 print(i)
                 i += 1
@@ -115,6 +92,6 @@ class API(unittest.TestCase):
     def test_save_to_repos_true(self):
         self.body(save_to_repos=True)
 
-    @e2e_pytest_api
-    def test_save_to_repos_false(self):
-        self.body(save_to_repos=False)
+    # @e2e_pytest_api
+    # def test_save_to_repos_false(self):
+    #     self.body(save_to_repos=False)
