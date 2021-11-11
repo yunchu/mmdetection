ec1fd5d156d78f80b0c204edd0b279148635fe34
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2021-11-11 19:15:25 +0300

    remove checks for model improvement

--- mmdet/apis/ote/apis/detection/train_task.py
+++ mmdet/apis/ote/apis/detection/train_task.py
@@ -23,8 +23,8 @@ import torch
 from ote_sdk.configuration import cfg_helper
 from ote_sdk.configuration.helper.utils import ids_to_strings
 from ote_sdk.entities.datasets import DatasetEntity
-from ote_sdk.entities.metrics import (CurveMetric, InfoMetric, LineChartInfo, LineMetricsGroup, MetricsGroup, Performance,
-                                      ScoreMetric, TextMetricsGroup, VisualizationInfo, VisualizationType)
+from ote_sdk.entities.metrics import (BarChartInfo, BarMetricsGroup, CurveMetric, LineChartInfo, LineMetricsGroup, MetricsGroup,
+                                      ScoreMetric, VisualizationType)
 from ote_sdk.entities.model import ModelEntity, ModelPrecision, ModelStatus
 from ote_sdk.entities.resultset import ResultSetEntity
 from ote_sdk.entities.subset import Subset
@@ -44,7 +44,7 @@ logger = logging.getLogger(__name__)
 
 class OTEDetectionTrainingTask(OTEDetectionInferenceTask, ITrainingTask):
 
-    def _generate_training_metrics_group(self, learning_curves) -> Optional[List[MetricsGroup]]:
+    def _generate_training_metrics(self, learning_curves, map) -> Optional[List[MetricsGroup]]:
         """
         Parses the mmdetection logs to get metrics from the latest training run
 
@@ -52,18 +52,19 @@ class OTEDetectionTrainingTask(OTEDetectionInferenceTask, ITrainingTask):
         """
         output: List[MetricsGroup] = []
 
-        # Model architecture
-        architecture = InfoMetric(name='Model architecture', value=self._model_name)
-        visualization_info_architecture = VisualizationInfo(name="Model architecture",
-                                                            visualisation_type=VisualizationType.TEXT)
-        output.append(TextMetricsGroup(metrics=[architecture],
-                                       visualization_info=visualization_info_architecture))
-
-        # Learning curves
+        # Learning curves.
         for key, curve in learning_curves.items():
             metric_curve = CurveMetric(xs=curve.x, ys=curve.y, name=key)
             visualization_info = LineChartInfo(name=key, x_axis_label="Epoch", y_axis_label=key)
             output.append(LineMetricsGroup(metrics=[metric_curve], visualization_info=visualization_info))
+
+        # Final mAP value on the validation set.
+        output.append(
+            BarMetricsGroup(
+                metrics=[ScoreMetric(value=map, name="mAP")],
+                visualization_info=BarChartInfo("Validation score", visualization_type=VisualizationType.RADIAL_BAR)
+            )
+        )
 
         return output
 
@@ -77,12 +78,8 @@ class OTEDetectionTrainingTask(OTEDetectionInferenceTask, ITrainingTask):
         val_dataset = dataset.get_subset(Subset.VALIDATION)
         config = self._config
 
-        # Create new model if training from scratch.
+        # Create a copy of the network.
         old_model = copy.deepcopy(self._model)
-
-        # Evaluate model performance before training.
-        _, initial_performance = self._infer_detector(self._model, config, val_dataset, dump_features=False, eval=True)
-        logger.info(f'initial_performance = {initial_performance}')
 
         # Check for stop signal between pre-eval and training. If training is cancelled at this point,
         # old_model should be restored.
@@ -106,6 +103,7 @@ class OTEDetectionTrainingTask(OTEDetectionInferenceTask, ITrainingTask):
         self._is_training = True
         self._model.train()
         train_detector(model=self._model, dataset=mm_train_dataset, cfg=training_config, validate=True)
+        logger.info("Training finished.")
 
         # Check for stop signal when training has stopped. If should_stop is true, training was cancelled and no new
         # model should be returned. Old train model is restored.
@@ -116,64 +114,49 @@ class OTEDetectionTrainingTask(OTEDetectionInferenceTask, ITrainingTask):
             self._is_training = False
             return
 
-        # Load the best weights and check if model has improved.
-        training_metrics = self._generate_training_metrics_group(learning_curves)
-        best_checkpoint_path = os.path.join(training_config.work_dir, 'latest.pth')
-        best_checkpoint = torch.load(best_checkpoint_path)
-        self._model.load_state_dict(best_checkpoint['state_dict'])
+        # Load best weights.
+        checkpoint_file_path = os.path.join(training_config.work_dir, 'best.pth')
+        if not os.path.isfile(checkpoint_file_path):
+            checkpoint_file_path = os.path.join(training_config.work_dir, 'latest.pth')
+        checkpoint = torch.load(checkpoint_file_path)
+        self._model.load_state_dict(checkpoint['state_dict'])
 
-        # Evaluate model performance after training.
-        val_preds, final_performance = self._infer_detector(self._model, config, val_dataset, dump_features=False, eval=True)
-        improved = final_performance > initial_performance
+        # Get predictions on the validation set.
+        val_preds, val_map = self._infer_detector(self._model, config, val_dataset, dump_features=False, eval=True)
+        preds_val_dataset = val_dataset.with_empty_annotations()
+        self._add_predictions_to_dataset(val_preds, preds_val_dataset, 0.0)
+        resultset = ResultSetEntity(
+            model=output_model,
+            ground_truth_dataset=val_dataset,
+            prediction_dataset=preds_val_dataset,
+        )
 
-        # Return a new model if model has improved, or there is no model yet.
-        if improved or self._task_environment.model is None:
-            if improved:
-                logger.info("Training finished, and it has an improved model")
-            else:
-                logger.info("First training round, saving the model.")
-            # Add mAP metric and loss curves
-            performance = Performance(score=ScoreMetric(value=final_performance, name="mAP"),
-                                      dashboard_metrics=training_metrics)
-            logger.info('FINAL MODEL PERFORMANCE (internal metric)\n' + str(performance))
-
-            preds_val_dataset = val_dataset.with_empty_annotations()
-            self._add_predictions_to_dataset(val_preds, preds_val_dataset, 0.0)
-
-            resultset = ResultSetEntity(
-                model=output_model,
-                ground_truth_dataset=val_dataset,
-                prediction_dataset=preds_val_dataset,
-            )
-
-            adaptive_threshold = self._hyperparams.postprocessing.result_based_confidence_threshold
-            if adaptive_threshold:
-                logger.info('Adjusting the confidence threshold')
-                metric = MetricsHelper.compute_f_measure(resultset, vary_confidence_threshold=True)
-                best_confidence_threshold = metric.best_confidence_threshold.value
-                if best_confidence_threshold is None:
-                    raise ValueError(f"Cannot compute metrics: Invalid confidence threshold!")
-                logger.info(f"Setting confidence threshold to {best_confidence_threshold} based on results")
-                self.confidence_threshold = best_confidence_threshold
-            else:
-                metric = MetricsHelper.compute_f_measure(resultset, vary_confidence_threshold=False)
-
-            if self.confidence_threshold is None:
-                logger.error('Confidence threshold is set to None. Falling back to the user defined value.')
-                self.confidence_threshold = self._hyperparams.postprocessing.confidence_threshold
-
-            performance = metric.get_performance()
-            logger.info('FINAL MODEL PERFORMANCE (external metric)\n' + str(performance))
-
-            self.save_model(output_model)
-            output_model.performance = performance
-            output_model.precision = [ModelPrecision.FP32]
-            output_model.model_status = ModelStatus.SUCCESS
+        # Adjust confidence threshold.
+        adaptive_threshold = self._hyperparams.postprocessing.result_based_confidence_threshold
+        if adaptive_threshold:
+            logger.info('Adjusting the confidence threshold')
+            metric = MetricsHelper.compute_f_measure(resultset, vary_confidence_threshold=True)
+            best_confidence_threshold = metric.best_confidence_threshold.value
+            if best_confidence_threshold is None:
+                raise ValueError(f"Cannot compute metrics: Invalid confidence threshold!")
+            logger.info(f"Setting confidence threshold to {best_confidence_threshold} based on results")
+            self.confidence_threshold = best_confidence_threshold
         else:
-            logger.info("Model performance has not improved while training. No new model has been saved.")
-            output_model.model_status = ModelStatus.NOT_IMPROVED
-            # Restore old training model if training from scratch and not improved
-            self._model = old_model
+            metric = MetricsHelper.compute_f_measure(resultset, vary_confidence_threshold=False)
+
+        if self.confidence_threshold is None:
+            logger.error('Confidence threshold is set to None. Falling back to the user defined value.')
+            self.confidence_threshold = self._hyperparams.postprocessing.confidence_threshold
+
+        # Compose performance statistics.
+        performance = metric.get_performance()
+        performance.dashboard_metrics.extend(self._generate_training_metrics(learning_curves, val_map))
+        logger.info(f'Final model performance: {str(performance)}')
+
+        # Save resulting model.
+        self.save_model(output_model)
+        output_model.performance = performance
+        output_model.model_status = ModelStatus.SUCCESS
 
         self._is_training = False
 
@@ -186,6 +169,7 @@ class OTEDetectionTrainingTask(OTEDetectionInferenceTask, ITrainingTask):
             'confidence_threshold': self.confidence_threshold, 'VERSION': 1}
         torch.save(modelinfo, buffer)
         output_model.set_data("weights.pth", buffer.getvalue())
+        output_model.precision = [ModelPrecision.FP32]
 
 
     def cancel_training(self):
