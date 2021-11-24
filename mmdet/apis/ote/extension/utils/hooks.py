76f94d7ec6ea0dd9b16070b9e3b3451e3a072875
Zalesskaya, Galina <galina.zalesskaya@intel.com>
2021-11-24 21:40:46 +0300

    get current lr from optimizer (instead of initial lr)

--- mmdet/apis/ote/extension/utils/hooks.py
+++ mmdet/apis/ote/extension/utils/hooks.py
@@ -478,3 +478,10 @@ class ReduceLROnPlateauLrUpdaterHook(LrUpdaterHook):
                 logger=runner.logger)
             self.current_lr = max(self.current_lr * self.factor, self.min_lr)
         return self.current_lr
+
+    def before_run(self, runner):
+        # TODO: remove overloaded method after fixing the issue
+        #  https://github.com/open-mmlab/mmdetection/issues/6572
+        self.base_lr = [
+            group['lr'] for group in runner.optimizer.param_groups
+        ]
