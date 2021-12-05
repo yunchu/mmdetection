e18b8b8c06cea5e1e8047ffd15e8a23a576e5d1c
Leonid Beynenson <Leonid.Beynenson@intel.com>
2021-12-05 16:54:01 +0300

    Move some fixtures and common pytest code to OTE SDK test helpers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,12 @@
-try:
-    import e2e.fixtures
+from ote_sdk.algo_backends.test_helpers.pytest_insertions import *
 
-    from e2e.conftest_utils import * # noqa
-    from e2e.conftest_utils import pytest_addoption as _e2e_pytest_addoption # noqa
-    from e2e import config # noqa
-    from e2e.utils import get_plugins_from_packages
-    pytest_plugins = get_plugins_from_packages([e2e])
-except ImportError:
-    _e2e_pytest_addoption = None
-    pass
+pytest_plugins = get_pytest_plugins_from_ote()
 
-import config
+ote_conftest_insertion(default_repository_name='ote/training_extensions/external/mmdetection')
+
+# pytest magic
+def pytest_generate_tests(metafunc):
+    ote_pytest_generate_tests_insertion(metafunc)
 
 def pytest_addoption(parser):
-    if _e2e_pytest_addoption:
-        _e2e_pytest_addoption(parser)
-    parser.addoption('--dataset-definitions', action='store', default=None,
-                     help='Path to the dataset_definitions.yml file for tests that require datasets.')
-    parser.addoption('--test-usecase', action='store', default=None,
-                     help='Optional. If the parameter is set, it filters test_ote_training tests by usecase field.')
-    parser.addoption('--expected-metrics-file', action='store', default=None,
-                     help='Optional. If the parameter is set, it points the YAML file with expected test metrics.')
-
-    # TODO: remove this after updating CI scripts -- this parameter is not required any more
-    parser.addoption('--template-paths', action='store', default=None,
-                     help='Path to the template_paths.yml file for tests that require templates.')
+    ote_pytest_addoption_insertion(parser)
