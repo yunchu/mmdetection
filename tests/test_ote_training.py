5b77c4034ee77b19a43a2b1803cbabb634bd1c0f
Fedor Kutsepin <fedorx.kutsepin@intel.com>
2021-12-20 17:02:01 +0300

    [OTE Reallife training] Diopsis dataset 	modified:   expected_metrics/metrics_test_ote_training.yml 	modified:   test_ote_training.py

--- tests/test_ote_training.py
+++ tests/test_ote_training.py
@@ -112,7 +112,7 @@ class ObjectDetectionTrainingTestParameters(DefaultOTETestCreationParametersInte
                        'Custom_Object_Detection_Gen3_ATSS',
                        'Custom_Object_Detection_Gen3_SSD',
                     ],
-                    dataset_name=['bbcd', 'vitens'],
+                    dataset_name=['bbcd', 'diopsis'],
                     num_training_iters=KEEP_CONFIG_FIELD_VALUE,
                     batch_size=KEEP_CONFIG_FIELD_VALUE,
                     usecase=REALLIFE_USECASE_CONSTANT,
